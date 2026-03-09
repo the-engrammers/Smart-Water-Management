224db7e3f94b8f5d081a16e20def144cb39f0df5
Zineb El hamri <zinebelhamri654@gmail.com>
2026-03-09 15:08:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from ai_models.leak_detection import predict_leak
 
+@app.post("/predict/leak")
+def leak_prediction(sensor_data: list):
+
+    try:
+        result = predict_leak(leak_model, sensor_data)
+        return result
+
+    except Exception:
+        # fallback logique seuil
+        if sensor_data[0] > 100:
+            return {"leak": True, "method": "threshold"}
+        else:
+            return {"leak": False, "method": "threshold"}
